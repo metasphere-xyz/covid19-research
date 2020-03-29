4d74bbd6480ca6aba03c76d71bfb62e47a4bc708
Kikuo Emoto <kikuomax@gmail.com>
2020-03-29 17:03:04 +0900

    feat: register function log level
    - The log level of the register article function can be set via a new
      parameter `RegisterArticleLogLevel`.

--- backend/aws/store/register-function/index.py
+++ backend/aws/store/register-function/index.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
 
 
+# configures the logger
+ALL_LOG_LEVELS = [
+    'NOTSET',
+    'DEBUG',
+    'INFO',
+    'WARNING',
+    'ERROR',
+    'CRITICAL'
+]
+DEFAULT_LOG_LEVEL = 'INFO'
+LOG_LEVEL = os.getenv('LAMBDA_LOG_LEVEL', DEFAULT_LOG_LEVEL)
+LOG_LEVEL = (LOG_LEVEL in ALL_LOG_LEVELS) and LOG_LEVEL or DEFAULT_LOG_LEVEL
 LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(getattr(logging, LOG_LEVEL))
 
 
 def lambda_handler (event, context):
+    global LOGGER
     LOGGER.info('register-function invoked')
     return {
         'statusCode': 200,
